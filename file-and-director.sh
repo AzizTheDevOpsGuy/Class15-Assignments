3b16f306669eb671d8f9884b2a523e11f6577539
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-08 04:06:27 +0000

    syntax error correction

--- file-and-director.sh
+++ file-and-director.sh
@@ -3,16 +3,17 @@
 #this script takes a name from the user and tell them if it is and argument.
 
 read -p "Please enter your file or directory name: " name
-if [ -d$name ]
+if [ -d $name ]
 then 
 	echo "$name is a directory in you system. its premission are bellow"
 	ls -la $name
-elif [ -f$name ] 
+elif [ -f $name ] 
 then 
 	echo "$name is a file in your system. see below for the permission"
+	ls -la $name
 else
 	echo "$name does not exist in your system"
-	echo " GOOD BYE BYE "
+	echo -e "\e[032m GOOD BYE BYE  \e[00m"
 	sleep 4
 fi
 
