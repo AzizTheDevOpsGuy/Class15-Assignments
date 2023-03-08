9272f51505ed7b02592c9b43f7ecb6338e27142a
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-08 03:54:25 +0000

    creating file, added shebang and first line of code

---
+++ file-and-director.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this script takes a name from the user and tell them if it is and argument.
+
+read -p "Please enter your file or directory name: " name
+if [ -d$name ]
+then 
+	echo "$name is a directory in you system. its premission are bellow"
+	ls -la $name
+elif [ -f$name ] 
+then 
+	echo "$name is a file in your system. see below for the permission"
+
